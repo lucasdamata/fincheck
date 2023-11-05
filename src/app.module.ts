248977aea79b1cac5ca2783b9797f4e9595c9557
Lucas da Mata <lucasmsilva_022@outlook.com>
2023-11-05 10:36:30 -0300

    fix: remove imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
-import { ResourceModule } from './auth/resource/resource.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, ResourceModule, AuthModule],
+  imports: [UsersModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
